8a02c8658d436de04f52ac8aeeea4e1950d4d111
OOLMrStone <150834976+OOLMrStone@users.noreply.github.com>
2023-12-03 18:26:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class LoginWidget(QMainWindow):
         self.cur = self.con.cursor()
         self.login = self.login_line.text()
         self.password = self.password_line.text()
-        self.reg_btn.clicked.connect(self.add_new)
+        self.reg_btn.clicked.connect(self.register_user)
         self.login_btn.clicked.connect(self.log_in)
         self.pixmap = QPixmap('giga.jpg')
         self.image = QLabel(self)
@@ -35,7 +35,7 @@ class LoginWidget(QMainWindow):
         self.label.setStyleSheet("QLabel { color: white; }")
         self.status_lbl.setStyleSheet("QLabel { color: white; }")
 
-    def add_new(self):
+    def register_user(self):
         self.login = self.login_line.text()
         self.password = self.password_line.text()
         if not self.cur.execute("SELECT login FROM Accounts WHERE (login=?)", (self.login,)).fetchall():
@@ -43,7 +43,7 @@ class LoginWidget(QMainWindow):
                 self.cur.execute(f"INSERT INTO Accounts (login, password) VALUES (?, ?)",
                                  (self.login, self.password))
                 self.status_lbl.setText("Registration completed successfully!")
-                self.start(self.login)
+                self.run_main_window(self.login)
             else:
                 self.status_lbl.setText("Password has to contain at least 8 chars.")
         else:
@@ -60,11 +60,11 @@ class LoginWidget(QMainWindow):
             if self.cur.execute("SELECT password FROM Accounts WHERE (login=? AND password=?)",
                                 (self.login, self.password)).fetchall():
                 self.status_lbl.setText(f"Welcome, {self.login}!")
-                self.start(self.login)
+                self.run_main_window(self.login)
             else:
                 self.status_lbl.setText("Password or login is incorrect. Try again.")
 
-    def start(self, login):
+    def run_main_window(self, login):
         self.mn = Main(login)
         self.mn.show()
         ex.close()
@@ -82,77 +82,84 @@ class Main(QMainWindow):
         self.day = []
         self.fcon = sqlite3.connect("accounts.db")
         self.fcur = self.fcon.cursor()
-        self.menu_btn.clicked.connect(self.get_menu)
+        self.menu_btn.clicked.connect(self.create_menu)
         self.watch_btn.clicked.connect(self.draw_graph)
-        self.save_btn.clicked.connect(self.choose_file)
+        self.save_btn.clicked.connect(self.save_menu)
         self.add_btn.clicked.connect(self.add_food)
-        self.search_btn.clicked.connect(self.find)
+        self.search_btn.clicked.connect(self.find_food)
         self.clear_btn.clicked.connect(self.clear_day)
         self.confirm_btn.clicked.connect(self.confirm)
         self.goal_btn.clicked.connect(self.change_goal)
         self.table_btn.clicked.connect(self.view_database)
 
-    def get_menu(self):
-        def choose(alike='%', between=(0, 0)):
-            """
-            :param alike: name filter
-            :param between: (left border move, right border move)
-            :return: chosen food
-            """
+    def choose_food(self, day_kcal, alike='%', between=(0, 0)):
+        """
+        :param day_kcal: amount of kcal for day
+        :param alike: name filter
+        :param between: (deviation) to the left, to the right
+        :return: chosen food
+        """
 
-            if between[0] == 0:
-                btw_str = ''
-            else:
-                btw_str = f" AND Energ_Kcal BETWEEN {kcal_count / 6 + between[0]} AND {kcal_count / 4 + between[1]}"
+        result = None
+        repeat = 1
+        while result is None:
             try:
-                result = random.choice(self.fcur.execute(
+                if between[0] == 0:
+                    btw_str = ''
+                else:
+                    btw_str = f" AND Energ_Kcal BETWEEN {day_kcal / 6 + between[0]} AND {day_kcal / 4 + between[1]}"
+                result = (random.choice(self.fcur.execute(
                     f"""SELECT Shrt_Desc, Energ_Kcal FROM food 
-                    WHERE Shrt_Desc LIKE '{alike}'""" + btw_str).fetchall())
+                    WHERE Shrt_Desc LIKE '{alike}'""" + btw_str).fetchall()), repeat)
             except IndexError:
-                result = None
-            return result
+                day_kcal //= 2
+                repeat *= 2
+        return result
 
+    def create_menu(self):
         kcal_count = int(self.cal_getter.value())
-        if kcal_count < 200:
+        if kcal_count < 500:
             self.menu.clear()
             self.menu.appendPlainText("It will be hard for you to survive...")
             return
         menu = []
-        yogurt = choose('YOGURT%')
-        breakfast = choose('EGG%', (-70, 70))
+        yogurt = self.choose_food(kcal_count, 'YOGURT%')
+        breakfast = self.choose_food(kcal_count, 'EGG%', (-70, 70))
         if self.hp.isChecked():
-            lunch_1 = choose('%PROTEIN%', (-150, 70))
-            lunch_2 = choose('%PROTEIN%', (-70, 150))
+            lunch_1 = self.choose_food(kcal_count, '%PROTEIN%', (-150, 70))
+            lunch_2 = self.choose_food(kcal_count, '%PROTEIN%', (-70, 150))
             if self.veg.isChecked():
-                extra_1 = choose('%SALAD DRSNG.POPPYSEED.CREAMY%')
+                extra_1 = self.choose_food(kcal_count, '%SALAD DRSNG.POPPYSEED.CREAMY%')
             else:
-                extra_1 = choose('%CHICKEN%', (-50, 50))
-            extra_2 = choose('%BEANS%', (-50, 50))
+                extra_1 = self.choose_food(kcal_count, '%CHICKEN%', (-50, 50))
+            extra_2 = self.choose_food(kcal_count, '%BEANS%', (-50, 50))
         else:
             if self.veg.isChecked():
-                lunch_1 = choose('%VEGETARIAN%', (-70, 70))
-                lunch_2 = choose('%VEGETARIAN%', (-70, 70))
-                extra_1 = choose('%YOGURT%', (-50, 50))
-                extra_2 = choose('NOODLES.EGG%', (-50, 50))
+                lunch_1 = self.choose_food(kcal_count, '%VEGETARIAN%', (-70, 70))
+                lunch_2 = self.choose_food(kcal_count, '%VEGETARIAN%', (-70, 70))
+                extra_1 = self.choose_food(kcal_count, '%YOGURT%', (-50, 50))
+                extra_2 = self.choose_food(kcal_count, 'NOODLES.EGG%', (-50, 50))
             else:
-                lunch_1 = choose('SOUP%', (-70, 70))
-                lunch_2 = choose('%STEAK%', (-40, 150))
-                extra_1 = choose('%CHICKEN%', (-50, 50))
-                extra_2 = choose('%BEANS%', (-50, 50))
-        dinner = choose('RICE%', (-40, 100))
+                lunch_1 = self.choose_food(kcal_count, 'SOUP%', (-70, 70))
+                lunch_2 = self.choose_food(kcal_count, '%STEAK%', (-40, 150))
+                extra_1 = self.choose_food(kcal_count, '%CHICKEN%', (-50, 50))
+                extra_2 = self.choose_food(kcal_count, '%BEANS%', (-50, 50))
+        dinner = self.choose_food(kcal_count, 'RICE%', (-40, 100))
         menu.extend([yogurt, breakfast, lunch_1, lunch_2, dinner, extra_1, extra_2])
+        kcal_count_copy = kcal_count
         for food in range(len(menu)):
-            kcal_count -= menu[food][1]
-            if kcal_count < 0:
+            kcal_count_copy -= menu[food][0][1] * menu[food][1]
+            if abs(kcal_count_copy) < max(100.0, kcal_count * 0.1):
                 index = food + 1
                 break
         else:
             index = len(menu)
         self.menu.clear()
         for food in menu[:index]:
-            if food is not None:
-                self.menu.appendPlainText(f"{food[0]} - {food[1]} calories\n")
-        self.menu.appendPlainText(f"Total: {int(self.cal_getter.value()) - kcal_count} calories.")
+            if food[0] is not None:
+                for _ in range(food[1]):
+                    self.menu.appendPlainText(f"{food[0][0]} - {food[0][1]} calories\n")
+        self.menu.appendPlainText(f"Total: {kcal_count - kcal_count_copy} calories.")
 
     def draw_graph(self):
         self.update()
@@ -186,13 +193,13 @@ class Main(QMainWindow):
         if self.goal:
             plt.addLine(y=self.goal, pen='y')
 
-    def choose_file(self):
+    def save_menu(self):
         fname = QFileDialog.getOpenFileName(self, 'Choose file:', '')[0]
         if fname:
             with open(fname, 'w') as nf:
                 nf.write(self.menu.toPlainText())
 
-    def add_food(self):
+    def add_food(self):  # adds user-selected food to "day" variable
         chosen_food = self.fcur.execute(
             f"""SELECT Shrt_Desc, Energ_Kcal FROM food 
             WHERE Shrt_Desc LIKE '{self.comboBox.currentText().replace("'", "_")}'""").fetchone()
@@ -201,7 +208,7 @@ class Main(QMainWindow):
         for line in self.day:
             self.menu.appendPlainText(f'{line[0]} - {line[1]} calories\n')
 
-    def find(self):
+    def find_food(self):  # finds user-selected food to add it to the comboBox
         alike = self.search.text().upper()
         found = self.fcur.execute(f"""SELECT Shrt_Desc FROM food
                                       WHERE Shrt_Desc LIKE '%{alike.replace("'", '_')}%'""").fetchall()
@@ -212,7 +219,8 @@ class Main(QMainWindow):
         self.menu.clear()
         self.day.clear()
 
-    def cpypt(self, date):
+    # need these two functions below because database is unable to work with number-alike headers
+    def encpypt(self, date):
         crypto = {'0': 'a',
                   '1': 'b',
                   '2': 'c',
@@ -248,8 +256,8 @@ class Main(QMainWindow):
             result += crypto[char]
         return result
 
-    def confirm(self):
-        my_date = self.cpypt(str(self.date_add.date().toPyDate()).replace('-', '_'))
+    def confirm(self):  # saves "day" variable to a database
+        my_date = self.encpypt(str(self.date_add.date().toPyDate()).replace('-', '_'))
         data = sum(int(product.split()[::-1][1]) for product in self.menu.toPlainText().split('\n\n'))
 
         self.fcur.execute("PRAGMA table_info('accounts')")
